c58c0a2b0f748fe0a8661c947643e931f4ac18b8
Richardalmeida21 <richard.c.dealmeida@gmail.com>
2026-04-17 18:12:50 -0300

    atualiza tamanho da imagem Open Graph para 1200x630 e ajusta layout do componente

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/opengraph-image",
-        width: 600,
-        height: 600,
+        width: 1200,
+        height: 630,
         alt: "Logo Igreja Ramo da Vida",
       },
     ],
--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -3,7 +3,7 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 export const alt = "Igreja Ramo da Vida";
-export const size = { width: 600, height: 600 };
+export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
 export default function Image() {
@@ -14,22 +14,77 @@ export default function Image() {
     (
       <div
         style={{
-          width: "600px",
-          height: "600px",
+          width: "1200px",
+          height: "630px",
           background: "#ffffff",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
+          gap: "64px",
+          padding: "60px 80px",
+          fontFamily: "serif",
         }}
       >
+        {/* Logo */}
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src={logoBase64}
-          width={420}
-          height={420}
+          width={220}
+          height={220}
           alt="logo"
-          style={{ objectFit: "contain" }}
+          style={{ objectFit: "contain", flexShrink: 0 }}
         />
+
+        {/* Divider */}
+        <div
+          style={{
+            width: "2px",
+            height: "220px",
+            background: "#d4a012",
+            flexShrink: 0,
+          }}
+        />
+
+        {/* Text */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "12px",
+          }}
+        >
+          <span
+            style={{
+              fontSize: "14px",
+              letterSpacing: "0.35em",
+              textTransform: "uppercase",
+              color: "#d4a012",
+              fontFamily: "sans-serif",
+            }}
+          >
+            COMUNIDADE CRISTÃ
+          </span>
+          <span
+            style={{
+              fontSize: "56px",
+              fontWeight: "700",
+              color: "#1a0a2e",
+              lineHeight: 1.1,
+            }}
+          >
+            Igreja Ramo{"\n"}da Vida
+          </span>
+          <span
+            style={{
+              fontSize: "22px",
+              color: "#6b7280",
+              fontFamily: "sans-serif",
+              marginTop: "8px",
+            }}
+          >
+            Campinas, SP
+          </span>
+        </div>
       </div>
     ),
     { ...size }
